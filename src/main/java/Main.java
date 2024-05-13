9472d5c1fa5c068a187efca1ef8160dc5286fd5e
Vitor <169068391+VitorSouza0709@users.noreply.github.com>
2024-05-13 21:33:29 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("a minha série favorita é: La casa de Papel");
+    System.out.println("");
+    System.out.println("sinospe:");
+    System.out.println("");
+    System.out.println("Oito assaltantes se trancam com reféns na Casa da Moeda da Espanha enquanto seu líder manipula a polícia. Será o maior roubo da história ou uma missão em vão?");
+    System.out.println("");
+    System.out.println("aqui está um link do trailer da série: https://youtu.be/iS5xXr-GOnM?si=jYTdkMP2aD_RfbBY");
+  }
+
+
+
+}
